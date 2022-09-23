704ccc6a8f22725d47e2e949dfa07b72af241671
melekabbassi <abbassimelek@gmail.com>
2022-09-23 15:56:07 +0100

    fix servlet package from testServlet.java

--- target/demo/servlets/com/demo/servlets/com/demo/servlets/com/demo/servlets/testServlet.java
+++ target/demo/servlets/com/demo/servlets/com/demo/servlets/com/demo/servlets/testServlet.java
@@ -1,11 +1,11 @@
-package servlets;   
+package com.demo.servlets;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 import java.io.IOException;
-
+import java.io.PrintWriter;
 
 class testServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
---
+++ target/demo/test.jsp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Test</title>
+    </head>
+    <body>
+        <p>This page is generated from a servlet</p>
+    </body>
+</html>
